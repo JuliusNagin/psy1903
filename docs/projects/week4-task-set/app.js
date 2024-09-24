b440c27d8ff9133b69265e1c24363da1fca46c04
Julius Nagin <jnagin@college.harvard.edu>
2024-09-24 16:23:01 -0400

    Question 4

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,5 +1,21 @@
 //Question #3 
-function celsiusToFahrenheit(celsius) {
-    return (celsius * 1.8) + 32;
+// function celsiusToFahrenheit(celsius) {
+//     return (celsius * 1.8) + 32;
+// }
+// console.log(celsiusToFahrenheit(10)); //returned 50
+//Question 4
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'c') {
+        return (temp - 32) / 1.8;
+    }
+    if (convertTo == 'f') {
+        return (temp * 1.8) + 32;
+    } //could also use 'else,' but that would return a Fahrenheit result to anything besides a 'c' in the second argument
 }
-console.log(celsiusToFahrenheit(10)); //returned 50
+
+console.log(convertTemp(10, 'c')); //returns 12.22
+console.log(convertTemp(10, 'f')); //returns 50
+
+
+
