b5729ac1f5e6f4741303a502ac496ff95e938721
Julius Nagin <jnagin@college.harvard.edu>
2024-09-24 17:51:22 -0400

    Question #6

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -18,16 +18,32 @@
 // console.log(convertTemp(10, 'f')); //returns 50
 
 //Question 5
+// let results = []
+// function getWordLengths(words) {
+//     for (let word of words) {
+//         results.push(word.length);
+//     }
+//     return (results)
+// }
+// let words = ['apple', 'banana', 'cherry', 'pear', 'grape'];
+// console.log(getWordLengths(words)); // Expected output: [5, 6, 6, 4, 5]
 
-let results = []
-function getWordLengths(words) {
+//Question 6
+
+function getLongestWord() {
+    let longestWord = '';
     for (let word of words) {
-        results.push(word.length);
+        if (word.length > longestWord.length) {
+            longestWord = word
+        }
     }
-    return (results)
+    return longestWord;
 }
+
 let words = ['apple', 'banana', 'cherry', 'pear', 'grape'];
-console.log(getWordLengths(words)); // Expected output: [5, 6, 6, 4, 5]
+console.log(words);
+
+console.log(getLongestWord(words));
 
 
 
