bd841711253c8c108f51d8f3e396821c14b791db
Julius Nagin <jnagin@college.harvard.edu>
2024-10-01 17:42:13 -0400

    Question #5

--- docs/projects/mrt-jspsych/experiment.js
+++ docs/projects/mrt-jspsych/experiment.js
@@ -2,43 +2,61 @@ let jsPsych = initJsPsych();
 
 let timeline = [];
 
-// Welcome 
+// Welcome & General Instructions 
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `<h1>Welcome to the Math Response Time Task </h1>
     <p>In this experiment, you will be shown a series of math questions.</p>
-    <p>Please answer as quickly and as accurately as possible</p>
+    <p>There are three parts to this experiment; the questions will increase in difficulty with each part.</p>
+    <p>Please answer as quickly and as accurately as possible.</p>
     <p>Press SPACE to begin</p>
     `,
     choices: [' ']
 };
 timeline.push(welcomeTrial);
 
-//Trial
+//Block 1
 
-for (let condition of conditions) {
-    let conditionTrial = {
-        type: jsPsychSurveyHtmlForm,
-        preamble: `<h1> What is ${condition.num1} + ${condition.num2}</h1>`,
-        html: `<p><input type='text' name='response' id='response'></p>`,
-        autofocus: 'response',
-        button_label: 'Submit Answer',
-        data: {
-            collect: true,
-            answer: condition.answer,
-            num1: condition.num1,
-            num2: condition.num2,
-        },
-        on_finish: function (data) {
-            data.response = data.response.response;
-            if (data.response == condition.answer) {
-                data.correct = true;
-            } else {
-                data.correct = false;
+for (let block of conditions) {
+
+    let blockIntroTrial = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: `
+            <h1>${block.title}</h1>
+            <p>Press SPACE to begin.</p>
+            `,
+        choices: [' '],
+    };
+
+    timeline.push(blockIntroTrial);
+
+    let blockConditions = block.questions;
+
+    for (let condition of blockConditions) {
+        let conditionTrial = {
+            type: jsPsychSurveyHtmlForm,
+            preamble: `<h1> What is ${condition.num1} + ${condition.num2}</h1>`,
+            html: `<p><input type='text' name='response' id='response'></p>`,
+            autofocus: 'response',
+            button_label: 'Submit Answer',
+            data: {
+                collect: true,
+                answer: condition.answer,
+                num1: condition.num1,
+                num2: condition.num2,
+                block: block.title,
+            },
+            on_finish: function (data) {
+                data.response = data.response.response;
+                if (data.response == condition.answer) {
+                    data.correct = true;
+                } else {
+                    data.correct = false;
+                }
             }
         }
+        timeline.push(conditionTrial);
     }
-    timeline.push(conditionTrial);
 }
 
 // Debrief
