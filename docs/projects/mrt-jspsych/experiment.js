143c2843c4d78727796b30628ebe727269757f45
Julius Nagin <jnagin@college.harvard.edu>
2024-10-01 17:11:57 -0400

    Question #4 MRT

--- docs/projects/mrt-jspsych/experiment.js
+++ docs/projects/mrt-jspsych/experiment.js
@@ -2,34 +2,58 @@ let jsPsych = initJsPsych();
 
 let timeline = [];
 
-let ageTrial = {
-    type: jsPsychSurveyHtmlForm,
-    preamble: '<p>How old are you?</p>',
-    html: `<p><input type='text' name='age' id='age'></p>`,
-    autofocus: 'age', // id of the field we want to auto-focus on when the trial starts
-    button_label: 'Submit Answer',
-    data: {
-        collect: true,
-    },
-    on_finish: function (data) {
-        data.age = data.response.age;
+// Welcome 
+let welcomeTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `<h1>Welcome to the Math Response Time Task </h1>
+    <p>In this experiment, you will be shown a series of math questions.</p>
+    <p>Please answer as quickly and as accurately as possible</p>
+    <p>Press SPACE to begin</p>
+    `,
+    choices: [' ']
+};
+timeline.push(welcomeTrial);
+
+//Trial
+
+for (let condition of conditions) {
+    let conditionTrial = {
+        type: jsPsychSurveyHtmlForm,
+        preamble: `<h1> What is ${condition.num1} + ${condition.num2}</h1>`,
+        html: `<p><input type='text' name='response' id='response'></p>`,
+        autofocus: 'response',
+        button_label: 'Submit Answer',
+        data: {
+            collect: true,
+            answer: condition.answer,
+            num1: condition.num1,
+            num2: condition.num2,
+        },
+        on_finish: function (data) {
+            data.response = data.response.response;
+            if (data.response == condition.answer) {
+                data.correct = true;
+            } else {
+                data.correct = false;
+            }
+        }
     }
+    timeline.push(conditionTrial);
 }
-timeline.push(ageTrial);
 
 // Debrief
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1>Thank you!</h1>
-    <p>You can now close this tab.</p>
+        <h1>Thank you!</h1>
+        <p>You can now close this tab.</p>
     `,
     choices: ['NO KEYS'],
     on_start: function () {
         let data = jsPsych.data
             .get()
             .filter({ collect: true })
-            .ignore(['response', 'stimulus', 'trial_type', 'trial_index', 'plugin_version', 'collect'])
+            .ignore(['collect', 'trial_type', 'trial_index', 'plugin_version',])
             .csv();
         console.log(data);
     }
