13499290931e39d0e8bce0cc1459d6dea34cc2f1
Julius Nagin <jnagin@college.harvard.edu>
2024-10-08 17:11:40 -0400

    Week 6 Question #4

--- docs/projects/mrt-jspsych/experiment.js
+++ docs/projects/mrt-jspsych/experiment.js
@@ -80,6 +80,65 @@ for (let block of conditions) {
     }
 }
 
+//Week 6 Task set: collecting results 
+let resultsTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['NO KEYS'],
+    async: false,
+    stimulus: function () {
+        return `
+                <h1>Please wait...</h1>
+                <span class='loader'></span>
+                <p>We are saving the results of your inputs.</p>
+            `;
+    },
+    on_start: function () {
+        //  ⭐ Update the following three values as appropriate ⭐
+        let prefix = 'MRT';
+        let dataPipeExperimentId = 'your-experiment-id-here';
+        let forceOSFSave = false;
+
+        // Filter and retrieve results as CSV data
+        let results = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
+            .csv();
+
+        console.log(results);
+
+        // Generate a participant ID based on the current timestamp
+        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+
+        // Dynamically determine if the experiment is currently running locally or on production
+        let isLocalHost = window.location.href.includes('localhost');
+
+        let destination = '/save';
+        if (!isLocalHost || forceOSFSave) {
+            destination = 'https://pipe.jspsych.org/api/data/';
+        }
+
+        // Send the results to our saving end point
+        fetch(destination, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify({
+                experimentID: dataPipeExperimentId,
+                filename: prefix + '-' + participantId + '.csv',
+                data: results,
+            }),
+        }).then(data => {
+            console.log(data);
+            jsPsych.finishTrial();
+        })
+    }
+}
+timeline.push(resultsTrial);
+
+
 // Debrief
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
@@ -88,14 +147,6 @@ let debriefTrial = {
         <p>You can now close this tab.</p>
     `,
     choices: ['NO KEYS'],
-    on_start: function () {
-        let data = jsPsych.data
-            .get()
-            .filter({ collect: true })
-            .ignore(['collect', 'trial_type', 'trial_index', 'plugin_version',])
-            .csv();
-        console.log(data);
-    }
 }
 timeline.push(debriefTrial);
 
